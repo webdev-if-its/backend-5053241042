3b5c54e0eb30448a91939ee733b444e5cac377c8
ItsYad <ziyadraziq01@gmail.com>
2026-09-15 17:16:19 +0700

    Pertemuan-03: Level 3 dan 5  selesai

--- pertemuan-03/main.go
+++ pertemuan-03/main.go
@@ -57,8 +57,16 @@ func LihatTugas(toko *TokoTugas, id int) (Task, error) {
 }
 
 func HapusTugas(toko *TokoTugas, id int) error {
-	panic("belum diimplementasikan")
+    for i, t := range toko.Daftar {
+        if t.ID == id {
+            toko.Daftar = append(toko.Daftar[:i], toko.Daftar[i+1:]...)
+            return nil
+        }
+    }
+    return ErrTugasTidakDitemukan
 }
+
+
 
 // HapusTugasTercatat memanggil HapusTugas, lalu memakai defer untuk
 // MENCATAT hasilnya ke toko.Log -- baik saat berhasil maupun saat gagal.
